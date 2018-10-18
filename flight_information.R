5c60b05339d258d5deecb642acbd0101e1c40730
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 14:01:17 -0400

    Saving flight_information.R

--- flight_information.R
+++ flight_information.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(ggplot2)
 DayofMonth <- flight_data[,c(1,14)]
 
+# Group by day of the month, report average satisfcation per group, coerce vector into df
 mean <- data.frame(tapply(as.numeric(DayofMonth$Satisfaction), DayofMonth$Day.of.Month, mean, na.rm = TRUE))
 colnames(mean) <- "Satisfaction"
 mean$Day.of.Month <- rownames(mean)
