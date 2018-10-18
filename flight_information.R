17906b5801f3fd8ae55b7fe4e0e0482304ec5f87
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 14:01:35 -0400

    Saving flight_information.R

--- flight_information.R
+++ flight_information.R
@@ -5,6 +5,7 @@ DayofMonth <- flight_data[,c(1,14)]
 
 # Group by day of the month, report average satisfcation per group, coerce vector into df
 mean <- data.frame(tapply(as.numeric(DayofMonth$Satisfaction), DayofMonth$Day.of.Month, mean, na.rm = TRUE))
+# Add titles, columns to df necessary for plotting
 colnames(mean) <- "Satisfaction"
 mean$Day.of.Month <- rownames(mean)
 barMean <- ggplot(mean, aes(x=reorder(Day.of.Month), y=Satisfaction, group = 1)) + geom_col() 
