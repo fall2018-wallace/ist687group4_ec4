beb67b0bd5ea5fe7045d4d97b43c90fc5d372ac2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-23 16:19:43 -0400

    Saving delay_analysis.R

--- delay_analysis.R
+++ delay_analysis.R
@@ -7,3 +7,5 @@ df <- data.frame(ssdata,stringsAsFactors = FALSE)
 #and how they affect our target variable i.e. 'Satisfaction'
 delay.df <- df[,c('Satisfaction','Scheduled Departure Hour','Departure Delay in Minutes',
                 'Arrival Delay in Minutes','Flight canceled','Arrival Delay greater 5 Mins')]
+
+library(ggplot2)
