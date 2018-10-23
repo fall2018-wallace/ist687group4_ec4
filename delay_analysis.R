4562ee0c4ad4bb6b6e76b92a5c3bcab147ea84c1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-23 16:16:30 -0400

    Saving delay_analysis.R

--- delay_analysis.R
+++ delay_analysis.R
@@ -1,3 +1,5 @@
 
 df <- data.frame(ssdata,stringsAsFactors = FALSE)
 head(df)
+
+#Scheduled Departure Hour, Departure Delay in Minutes, Arrival Delay in Minutes, Flight canceled, Arrival Delay greater 5 Mins 
