8f616aaa1bfe2fe3506fa9b76bf8228dd2cca37b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-23 16:19:33 -0400

    Saving delay_analysis.R

--- delay_analysis.R
+++ delay_analysis.R
@@ -1,9 +1,9 @@
 
 df <- data.frame(ssdata,stringsAsFactors = FALSE)
-head(df)
 
 #Scheduled Departure Hour, Departure Delay in Minutes, Arrival Delay in Minutes, 
 #Flight canceled, Arrival Delay greater 5 Mins 
 #First we subset our data to just look at the variables which depict flight delay
+#and how they affect our target variable i.e. 'Satisfaction'
 delay.df <- df[,c('Satisfaction','Scheduled Departure Hour','Departure Delay in Minutes',
                 'Arrival Delay in Minutes','Flight canceled','Arrival Delay greater 5 Mins')]
