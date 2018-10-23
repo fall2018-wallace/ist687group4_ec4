99eb9ae305786575feada008a0f5b9a84cf0ff09
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-23 16:18:57 -0400

    Saving delay_analysis.R

--- delay_analysis.R
+++ delay_analysis.R
@@ -4,5 +4,6 @@ head(df)
 
 #Scheduled Departure Hour, Departure Delay in Minutes, Arrival Delay in Minutes, 
 #Flight canceled, Arrival Delay greater 5 Mins 
-
-delay.df <- df[,c('Satisfaction','Scheduled Departure Hour','Departure Delay in Minutes',Arrival Delay in Minutes, Flight canceled, Arrival Delay greater 5 Mins )]
+#First we subset our data to just look at the variables which depict flight delay
+delay.df <- df[,c('Satisfaction','Scheduled Departure Hour','Departure Delay in Minutes',
+                'Arrival Delay in Minutes','Flight canceled','Arrival Delay greater 5 Mins')]
