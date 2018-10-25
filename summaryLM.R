59656817f4a813ad96c0be57b016cd4e98030026
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 17:14:28 -0400

    Saving summaryLM.R

--- summaryLM.R
+++ summaryLM.R
@@ -1,4 +1,5 @@
 
+# Create a linear model with all applicable variables
 summaryModel <- summary(lm(formula = SatisfactionNumeric ~ Airline.Status + Age + Gender + Price.Sensitivity + Year.of.First.Flight + No.of.Flights.p.a. + X..of.Flight.with.other.Airlines +
   Type.of.Travel + No..of.other.Loyalty.Cards + Shopping.Amount.at.Airport + Eating.and.Drinking.at.Airport +
   Class + Day.of.Month + Airline.Code + Scheduled.Departure.Hour + Departure.Delay.in.Minutes + Arrival.Delay.in.Minutes +
