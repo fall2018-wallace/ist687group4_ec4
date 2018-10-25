ea45d79fce4a2916166917eff05ad34a27e0fd1c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 17:14:36 -0400

    Saving summaryLM.R

--- summaryLM.R
+++ summaryLM.R
@@ -4,3 +4,5 @@ summaryModel <- summary(lm(formula = SatisfactionNumeric ~ Airline.Status + Age 
   Type.of.Travel + No..of.other.Loyalty.Cards + Shopping.Amount.at.Airport + Eating.and.Drinking.at.Airport +
   Class + Day.of.Month + Airline.Code + Scheduled.Departure.Hour + Departure.Delay.in.Minutes + Arrival.Delay.in.Minutes +
   Flight.Distance + Arrival.Delay.greater.5.Mins + Flight.time.in.minutes, data = cleanSatisfactionSurvey))
+
+# These were not applicable
