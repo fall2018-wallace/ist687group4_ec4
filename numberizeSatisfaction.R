6a440e38d6451da32cbe33b1b7c5e46dfb9b10aa
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 17:12:47 -0400

    Saving numberizeSatisfaction.R

--- numberizeSatisfaction.R
+++ numberizeSatisfaction.R
@@ -3,5 +3,6 @@
 # (we can do this check as an automated, non-manual process for the final project, but I knew what they were and am just exploring)
 cleanSatisfactionSurvey <- satisfactionSurvey[(satisfactionSurvey$Satisfaction!="4.00.2.00"&satisfactionSurvey$Satisfaction!="4.00.5"),]
 
+# Coerce satisfaction ratings into a new (numeric) column
 cleanSatisfactionSurvey$SatisfactionNumeric <- as.numeric(as.character(cleanSatisfactionSurvey$Satisfaction))
 cleanSatisfactionSurvey$binarySat <- cleanSatisfactionSurvey$SatisfactionNumeric > 3.5
