105ac1a6783f37eaab588e825b3e8308b00733f6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 16:23:15 -0400

    Saving numberizeSatisfaction.R

--- numberizeSatisfaction.R
+++ numberizeSatisfaction.R
@@ -1,1 +1,4 @@
 
+cleanSatisfactionSurvey <- satisfactionSurvey[(satisfactionSurvey$Satisfaction!="4.00.2.00"&satisfactionSurvey$Satisfaction!="4.00.5"),]
+cleanSatisfactionSurvey$SatisfactionNumeric <- as.numeric(as.character(cleanSatisfactionSurvey$Satisfaction))
+cleanSatisfactionSurvey$binarySat <- cleanSatisfactionSurvey$SatisfactionNumeric > 3.5
