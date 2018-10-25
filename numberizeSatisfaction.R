7b982fb0878be79b29bcb65684cfe5cedb96134c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 17:11:48 -0400

    Saving numberizeSatisfaction.R

--- numberizeSatisfaction.R
+++ numberizeSatisfaction.R
@@ -1,5 +1,6 @@
 
-# Remove the rows with non-numeric satisfaction ratings (we can do this as a less manual process later, but I kn)
+# Remove the rows with non-numeric satisfaction ratings 
+# (we can do this cehck as an automated, non-manual process later, but I knew what they were)
 cleanSatisfactionSurvey <- satisfactionSurvey[(satisfactionSurvey$Satisfaction!="4.00.2.00"&satisfactionSurvey$Satisfaction!="4.00.5"),]
 cleanSatisfactionSurvey$SatisfactionNumeric <- as.numeric(as.character(cleanSatisfactionSurvey$Satisfaction))
 cleanSatisfactionSurvey$binarySat <- cleanSatisfactionSurvey$SatisfactionNumeric > 3.5
