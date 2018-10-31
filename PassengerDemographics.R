58ae257dfbdb1b0f7e0fc9cecb6f6f039265714f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 19:35:21 -0400

    Saving PassengerDemographics.R

--- PassengerDemographics.R
+++ PassengerDemographics.R
@@ -1,4 +1,17 @@
 
+df <- data.frame(ssdata,stringsAsFactors = FALSE)
+
+#Initial look at NAs in our data
+for(i in colnames(df)){
+  print(paste(i,'=',sum(is.na(df[,i]))))
+}
+
+delay.df <- df[,c('Satisfaction','Scheduled.Departure.Hour','Departure.Delay.in.Minutes',
+                  'Arrival.Delay.in.Minutes','Flight.cancelled','Arrival.Delay.greater.5.Mins')]
+
+
+
+
 df <- data.frame('SatisfactionSurvey.csv', stringsAsFactors = FALSE)
 passengerdf <- df[,c("Age","Gender","Year.of.First.Flight","No.of.Flights.p.a.","Shopping.Amount.at.Airport","Price.Sensitivity","Eating.and.Drinking.at.Airport")]
 
