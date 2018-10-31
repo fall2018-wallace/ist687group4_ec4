ff1655b81c6e9112fd9c53baea0922e97653bdda
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 19:36:01 -0400

    Saving PassengerDemographics.R

--- PassengerDemographics.R
+++ PassengerDemographics.R
@@ -2,7 +2,7 @@
 
 
 df <- data.frame('SatisfactionSurvey.csv', stringsAsFactors = FALSE)
-passengerdf <- df[,c("Age","Gender","Year.of.First.Flight","No.of.Flights.p.a.","Shopping.Amount.at.Airport","Price.Sensitivity","Eating.and.Drinking.at.Airport")]
+passengerdf <- df[c("Age","Gender","Year.of.First.Flight","No.of.Flights.p.a.","Shopping.Amount.at.Airport","Price.Sensitivity","Eating.and.Drinking.at.Airport")]
 
 library(ggplot2)
 
