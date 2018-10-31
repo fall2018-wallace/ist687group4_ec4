a9f90fb066beb5c78c67518e5dd835debd23ec36
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 19:39:25 -0400

    Saving PassengerDemographics.R

--- PassengerDemographics.R
+++ PassengerDemographics.R
@@ -1,7 +1,7 @@
 
 
 
-df <- data.frame('SatisfactionSurvey.csv', stringsAsFactors = FALSE)
+df <- data.frame('satisfactionSurvey.csv', stringsAsFactors = FALSE)
 str(df)
 passengerdf <- df[c("Age","Gender","Year.of.First.Flight","No.of.Flights.p.a.","Shopping.Amount.at.Airport","Price.Sensitivity","Eating.and.Drinking.at.Airport")]
 
