e9b384827eb59797212da8b1bba49fcd90bed5b3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 17:00:50 -0400

    Saving estimateNULLs.R

--- estimateNULLs.R
+++ estimateNULLs.R
@@ -2,5 +2,7 @@
 # We are missing values for Arrival Delay and Flight Time for flights that we know took place.
 # To see if the data that we do have can be used to accurately estimate the missing values,
 # we will run a linear regression on the missing variables and those related to them.
+
+# Flight Time should be related to the distance travelled with variances occurring due to things like 
 predictModel <- lm(formula = Flight.time.in.minutes ~ Flight.Distance, data = ssClean)
 summary(predictModel)
