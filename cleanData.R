111c0faa842a3d690ef66e51f4c244f45f37f3d9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 13:39:24 -0400

    Saving cleanData.R

--- cleanData.R
+++ cleanData.R
@@ -28,3 +28,5 @@ nrow(ssRaw[(ssRaw$Satisfaction=="4.00.2.00"|ssRaw$Satisfaction=="4.00.5"),]) / n
 # Remove the rows with non-numeric satisfaction ratings 
 ssCleanSat <- ssRaw[(ssRaw$Satisfaction!="4.00.2.00"&ssRaw$Satisfaction!="4.00.5"),]
 
+
+
