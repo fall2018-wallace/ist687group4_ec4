294eab54194b86cd1606c670d583d3fc2aa06de8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 16:58:37 -0400

    Saving estimateNULLs.R

--- estimateNULLs.R
+++ estimateNULLs.R
@@ -1,3 +1,4 @@
 
+# We are missing values for Arrival Delay and Flight Time for flights that we know took place.
 predictModel <- lm(formula = Flight.time.in.minutes ~ Flight.Distance, data = ssClean)
 summary(predictModel)
