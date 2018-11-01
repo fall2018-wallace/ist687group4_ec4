946005710f3f2bd14fe5eba38cf4d4fc80ed6161
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 14:25:06 -0400

    Saving cleanData.R

--- cleanData.R
+++ cleanData.R
@@ -63,8 +63,8 @@ dfBlankCounts
 # Set Departure Delays, Arrival Delays and Flight Time to zero where they are blank and 
 # where Flight Cancelled is "Yes"
 
-ssCleanSat$Departure.Delay.in.Minutes <- ifelse (is.na(ssCleanSat$Departure.Delay.in.Minutes)&ssCleanSat$Flight.cancelled=="Yes", 0, ssCleanSat$Departure.Delay.in.Minutes)
-ssCleanSat$Arrival.Delay.in.Minutes <- ifelse (is.na(ssCleanSat$Arrival.Delay.in.Minutes)&ssCleanSat$Flight.cancelled=="Yes", 0, ssCleanSat$Arrival.Delay.in.Minutes)
-ssCleanSat$Flight.time.in.minutes <- ifelse (is.na(ssCleanSat$Departure.Delay.in.Minutes)&ssCleanSat$Flight.cancelled=="Yes", 0, ssCleanSat$Departure.Delay.in.Minutes)
-# ean <- ssClean[(is.na(ssClean$Arrival.Delay.in.Minutes)&ssClean$Flight.cancelled=="Yes"),] <- 0
-# ssClean <- ssClean[(is.na(ssClean$Flight.time.in.minutes)&ssClean$Flight.cancelled=="Yes"),] <- 0
+ssClean <- ssCleanSat
+ssClean$Departure.Delay.in.Minutes <- ifelse (is.na(ssClean$Departure.Delay.in.Minutes)&ssClean$Flight.cancelled=="Yes", 0, ssClean$Departure.Delay.in.Minutes)
+ssClean$Arrival.Delay.in.Minutes <- ifelse (is.na(ssClean$Arrival.Delay.in.Minutes)&ssClean$Flight.cancelled=="Yes", 0, ssClean$Arrival.Delay.in.Minutes)
+ssClean$Flight.time.in.minutes <- ifelse (is.na(ssClean$Flight.time.in.minutes)&ssClean$Flight.cancelled=="Yes", 0, ssClean$Flight.time.in.minutes)
+
