5ea6bf9549fd1344ad482f7d8d857cd8e4a765f6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 14:39:14 -0400

    Saving cleanData.R

--- cleanData.R
+++ cleanData.R
@@ -6,7 +6,7 @@
 # unexpected values not equal to a number between 0 and 5.
 unique(ssRaw$Satisfaction)
 
-# Per the above analysis, we see that there are some Satisfaction ratings of 4.00.5 and 
+# Per the above output, we see that there are some Satisfaction ratings of 4.00.5 and 
 # 4.00.2.00 in the data which fall outside of our expected format and are non-numeric 
 # (unable to be used in numerical analysis). As these are most likely errors, let's check
 # to see how much of the data these errors affect.
@@ -20,7 +20,7 @@ nrow(ssRaw)
 # Display the number of non-standard rows as a percentage of total rows
 nrow(ssRaw[(ssRaw$Satisfaction=="4.00.2.00"|ssRaw$Satisfaction=="4.00.5"),]) / nrow(ssRaw) * 100
 
-# As these numbers are unable to be analyzed, we can not be sure of what numbers they 
+# Taking into account the fact that these numbers are unable to be analyzed as entered andwe can not be sure of what numbers they 
 # were intended to be and they make up 3 out of 129889 (or 0.002%) of entries and wont' 
 # significantly influence overall trends, our group has opted to remove them completely 
 # from the data set before moving forward. 
