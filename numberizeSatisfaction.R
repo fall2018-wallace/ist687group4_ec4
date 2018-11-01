1870d3323b8fe35097486fc0747c9ba339ca6fe3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 15:39:15 -0400

    Saving numberizeSatisfaction.R

--- numberizeSatisfaction.R
+++ numberizeSatisfaction.R
@@ -11,6 +11,8 @@ summary(ssClean$SatisfactionNumeric)
 histSat <- hist(ssClean$SatisfactionNumeric)
 
 # It seems the Satisfaction numbers fall into two groups distributed around 3.5, so we will
-# make that our threshold for defining "High" and "Low" satisfaction. We know from previous analysis
+# make that our threshold for defining "High" and "Low" satisfaction. We know from previous 
+# analysis that there is an insignificant quantity of 3.5 ratings so we will group them in the 
+
 # Create a new column assigning a binary value to satisfaction for future analysis
 ssClean$binarySat <- ssClean$SatisfactionNumeric > 3.5
