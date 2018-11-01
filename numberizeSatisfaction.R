4a29e984ac9fd5aee6c107ebebe3663e1ccfc5e4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 15:01:18 -0400

    Saving numberizeSatisfaction.R

--- numberizeSatisfaction.R
+++ numberizeSatisfaction.R
@@ -1,6 +1,6 @@
 
 # Coerce satisfaction ratings into a new (numeric) column
-cleanSatisfactionSurvey$SatisfactionNumeric <- as.numeric(as.character(cleanSatisfactionSurvey$Satisfaction))
+ssClean$SatisfactionNumeric <- as.numeric(as.character(ssClean$Satisfaction))
 
 # Create a new column assigning a binary value to satisfaction for future analysis
-cleanSatisfactionSurvey$binarySat <- cleanSatisfactionSurvey$SatisfactionNumeric > 3.5
+ssClean$binarySat <- ssClean$SatisfactionNumeric > 3.5
