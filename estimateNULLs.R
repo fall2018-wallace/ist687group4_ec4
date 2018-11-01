9ed2a08d3fc8dfa796e908655b13d7fe3f940808
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 17:01:23 -0400

    Saving estimateNULLs.R

--- estimateNULLs.R
+++ estimateNULLs.R
@@ -3,6 +3,7 @@
 # To see if the data that we do have can be used to accurately estimate the missing values,
 # we will run a linear regression on the missing variables and those related to them.
 
-# Flight Time should be related to the distance travelled with variances occurring due to things like 
+# Flight Time should be related to the distance travelled with variances occurring due to things 
+# like head and tail winds, flight direction, etc. We'll use a linear model to see if these 
 predictModel <- lm(formula = Flight.time.in.minutes ~ Flight.Distance, data = ssClean)
 summary(predictModel)
