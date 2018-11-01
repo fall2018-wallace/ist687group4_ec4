3791bc9a5ff9199052505f57cfecc2e726da3dde
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 13:13:19 -0400

    Saving cleanData.R

--- cleanData.R
+++ cleanData.R
@@ -1,4 +1,5 @@
 
+# The Satisfaction variable is our dependent variable so it is important that it is 
 # Remove the rows with non-numeric satisfaction ratings 
 # Per the group's decision: the we can do this check as an automated, non-manual process for the final project, but I knew what they were and am just exploring)
 cleanSatisfactionSurvey <- satisfactionSurvey[(satisfactionSurvey$Satisfaction!="4.00.2.00"&satisfactionSurvey$Satisfaction!="4.00.5"),]
