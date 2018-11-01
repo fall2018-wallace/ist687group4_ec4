4f2dcb023e2f923e4391b5f5f691930f0963e65f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 15:24:38 -0400

    Saving numberizeSatisfaction.R

--- numberizeSatisfaction.R
+++ numberizeSatisfaction.R
@@ -4,5 +4,6 @@ ssClean$SatisfactionNumeric <- as.numeric(as.character(ssClean$Satisfaction))
 
 # In order to define our binary satisfcation categories of "High" and "Low", we must determine
 # the threshold that makes the most sense within the distribution of Satisfaction numbers 
+# in our data.
 # Create a new column assigning a binary value to satisfaction for future analysis
 ssClean$binarySat <- ssClean$SatisfactionNumeric > 3.5
