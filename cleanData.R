fd6da158af699068083cb7de2e2e1cea776700ad
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 13:25:25 -0400

    Saving cleanData.R

--- cleanData.R
+++ cleanData.R
@@ -20,6 +20,7 @@ nrow(ssRaw)
 # Display the number of non-standard rows as a percentage of total rows
 nrow(ssRaw[(ssRaw$Satisfaction=="4.00.2.00"|ssRaw$Satisfaction=="4.00.5"),]) / nrow(ssRaw) * 100
 
+# As these numbers are unable to be analyazed, we can not be sure of what numbers they 
 # Remove the rows with non-numeric satisfaction ratings 
 # Per the group's decision: the we can do this check as an automated, non-manual process for the final project, but I knew what they were and am just exploring)
 # cleanSatisfactionSurvey <- satisfactionSurvey[(satisfactionSurvey$Satisfaction!="4.00.2.00"&satisfactionSurvey$Satisfaction!="4.00.5"),]
