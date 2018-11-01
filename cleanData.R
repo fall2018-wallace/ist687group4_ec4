7aa9f7c79087e770455ec74ce5fc6ab227d5521b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 13:16:39 -0400

    Saving cleanData.R

--- cleanData.R
+++ cleanData.R
@@ -6,6 +6,8 @@
 # between 0 and 5.
 unique(ssRaw$Satisfaction)
 
+# Per the above analysis, we see that there are some Satisfaction ratings of 4.00.5 and 
+
 # Remove the rows with non-numeric satisfaction ratings 
 # Per the group's decision: the we can do this check as an automated, non-manual process for the final project, but I knew what they were and am just exploring)
 cleanSatisfactionSurvey <- satisfactionSurvey[(satisfactionSurvey$Satisfaction!="4.00.2.00"&satisfactionSurvey$Satisfaction!="4.00.5"),]
