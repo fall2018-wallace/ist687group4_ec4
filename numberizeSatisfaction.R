00b2defb455832cd86b6316c8ce5b3d2726da1f9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 15:23:42 -0400

    Saving numberizeSatisfaction.R

--- numberizeSatisfaction.R
+++ numberizeSatisfaction.R
@@ -2,5 +2,6 @@
 # Coerce satisfaction ratings into a new (numeric) column
 ssClean$SatisfactionNumeric <- as.numeric(as.character(ssClean$Satisfaction))
 
+# In order to define our binary satisfcation categories of "High" and "Low", we must determine
 # Create a new column assigning a binary value to satisfaction for future analysis
 ssClean$binarySat <- ssClean$SatisfactionNumeric > 3.5
