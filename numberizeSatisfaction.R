2df66c265ea3f7e382a30ae003501d7f74443c81
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 15:41:51 -0400

    Saving numberizeSatisfaction.R

--- numberizeSatisfaction.R
+++ numberizeSatisfaction.R
@@ -2,7 +2,7 @@
 # Coerce satisfaction ratings into a new (numeric) column for ease in future analysis
 ssClean$SatisfactionNumeric <- as.numeric(as.character(ssClean$Satisfaction))
 
-# In order to define binary satisfcation categories of "High" and "Low", we must determine
+# In order to define binary satisfaction categories of "High" and "Low", we must determine
 # the threshold that makes the most sense within the distribution of Satisfaction numbers 
 # in our data.
 
@@ -15,5 +15,5 @@ histSat <- hist(ssClean$SatisfactionNumeric)
 # analysis that there is an insignificant quantity of 3.5 ratings so we will group them in the 
 # high group for the sake of simplicity.
 
-# Create a new column assigning a binary value to satisfaction for future analysis
+# Create a new column assigning a binary value to satisfaction
 ssClean$binarySat <- ifelse(ssClean$SatisfactionNumeric > 3.5, "High", "Low")
