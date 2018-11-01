61c5d5daae6a4f2ed33223432414919abdb6a644
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 13:14:23 -0400

    Saving cleanData.R

--- cleanData.R
+++ cleanData.R
@@ -2,7 +2,7 @@
 # The Satisfaction variable is our dependent variable so it is important that it is 
 # formatted correctly and that its values are measurable.
 
-# Explore the data to discover any anomolies or unexpected values not equal to an 
+# Explore the data to discover any anomolies or unexpected values not equal to a number
 # Remove the rows with non-numeric satisfaction ratings 
 # Per the group's decision: the we can do this check as an automated, non-manual process for the final project, but I knew what they were and am just exploring)
 cleanSatisfactionSurvey <- satisfactionSurvey[(satisfactionSurvey$Satisfaction!="4.00.2.00"&satisfactionSurvey$Satisfaction!="4.00.5"),]
