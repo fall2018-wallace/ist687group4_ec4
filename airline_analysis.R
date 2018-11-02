da2136908ae8bf156da6be12715d62cd43ace578
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 21:24:15 -0400

    Saving airline_analysis.R

--- airline_analysis.R
+++ airline_analysis.R
@@ -24,6 +24,7 @@ statusBar <- ggplot(satisfactionSurvey,aes(x=Airline.Status, y=Satisfaction, gro
 statusBar <- statusBar + geom_col()
 statusBar <- statusBar + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 statusBar
+
 #Blue (lowest level) members have a signifcantly higher satisfaction rate
 
 
