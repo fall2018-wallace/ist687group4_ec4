a8b93717641daa96fb7185d8c4ba79ddbbf6f94d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-02 10:55:21 -0400

    Saving discretizingPart3.R

--- discretizingPart3.R
+++ discretizingPart3.R
@@ -14,7 +14,8 @@ defineBucketQuantiles <- function(inputVector)
   return(vBuckets) # Anything not assigned to "High" or "Low" will retain default value ("Average")
 }
 
-# Use the above function to discretize the variables (using the cleaned and/or estimated ones where)
+# Use the above function to discretize the variables (using the cleaned and/or estimated ones where
+# applicable)
 dHour <- defineBucketQuantiles(ssClean$Scheduled.Departure.Hour)
 dDepDelay <- defineBucketQuantiles(ssClean$Departure.Delay.in.Minutes.0)
 dArrDelay <- defineBucketQuantiles(ssClean$Arrival.Delay.in.Minutes.0.Est)
