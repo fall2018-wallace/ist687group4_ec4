ec363e03cc0ee0e27246df6258f327fb684d4720
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-08 14:05:03 -0500

    Saving combine.R

--- combine.R
+++ combine.R
@@ -1,4 +1,3 @@
 
 #Create a new data frame based on three different parts of discretized data (Need to add part 2)
 dSatisfactionCategory <- data.frame(cbind(part1,part2,part3,part4))
-discretizeddata <- write.csv(dSatisfactionCategory)
