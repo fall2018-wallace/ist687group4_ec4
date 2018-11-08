ef52ec4a7e6d8f37372dbc35096cf723b76737ca
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-08 12:19:42 -0500

    Saving modelling.R

--- modelling.R
+++ modelling.R
@@ -1,8 +1,8 @@
 
 #Modeling 
 
-library("arules")
-library("arulesViz")
+library(arules)
+library(arulesViz)
 
 #Create a new data frame based on three different parts of discretized data (Need to add part 2)
 dSatisfactionCategory <- data.frame(cbind(part1,part2,part3,part4))
