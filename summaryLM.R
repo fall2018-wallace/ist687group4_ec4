12a42644113905bced80f325282df9d8aaa1e706
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 09:42:26 -0500

    Saving summaryLM.R

--- summaryLM.R
+++ summaryLM.R
@@ -18,4 +18,5 @@ summaryModel <- summary(lm(formula = SatisfactionNumeric ~ Airline.Status + Age 
 summaryModel <- summary(lm(formula = SatisfactionNumeric ~ Airline.Status + Age + Gender + 
     Price.Sensitivity + Year.of.First.Flight + No.of.Flights.p.a. + X..of.Flight.with.other.Airlines +
     Type.of.Travel + No..of.other.Loyalty.Cards + Shopping.Amount.at.Airport + 
-    Eating.and.Drinking.at.Airport + Class + Day.of.Month + Flight.date + Airline.Code + Airline.Name +Scheduled.Departure.Hour+ Arrival.Delay.greater.5.Mins, data = cleanSatisfactionSurvey))
+    Eating.and.Drinking.at.Airport + Class + Day.of.Month + Flight.date + Airline.Code + Airline.Name +
+    Orgin.City + Origin.State + Destination.City + Destination.State + Scheduled.Departure.Hour + Arrival.Delay.greater.5.Mins, data = cleanSatisfactionSurvey))
