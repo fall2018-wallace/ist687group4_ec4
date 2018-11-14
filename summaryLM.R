0480555af81187837d5d10fb0147df333f637d9e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 10:08:54 -0500

    Saving summaryLM.R

--- summaryLM.R
+++ summaryLM.R
@@ -24,3 +24,13 @@ summaryModel2 <- summary(lm(formula = SatisfactionNumeric ~ Airline.Status + Age
     Flight.time.in.minutes.0.Est + Flight.Distance + Arrival.Delay.greater.5.Mins, 
     data = cleanSatisfactionSurvey))
 summaryModel2
+
+# These were not applicable or too specialized
+# Orgin.City + Origin.State + Destination.City + Destination.State + Flight.date + Airline.Name
+
+# These were not statistically significant or less significant
+# X..of.Flight.with.other.Airlines + No..of.other.Loyalty.Cards + Eating.and.Drinking.at.Airport +
+# Day.of.Month + Airline.Code + Departure.Delay.in.Minutes.0 + Arrival.Delay.in.Minutes.0.Est + 
+# Flight.time.in.minutes.0.Est + Flight.Distance
+
+# Re-run the linear model with the statistically significant variables
