ebc70ead833254beb3be4dfe4fcbb3ca34b5231e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 10:05:08 -0500

    Saving summaryLM.R

--- summaryLM.R
+++ summaryLM.R
@@ -11,8 +11,8 @@ summaryModel <- summary(lm(formula = SatisfactionNumeric ~ Airline.Status + Age 
 # Create a linear model with all applicable variables
 summaryModel2 <- summary(lm(formula = SatisfactionNumeric ~ Airline.Status + Age + Gender + 
     Price.Sensitivity + Year.of.First.Flight + No.of.Flights.p.a. +
-    Type.of.Travel + No..of.other.Loyalty.Cards + Shopping.Amount.at.Airport + 
-    Eating.and.Drinking.at.Airport + Class + Day.of.Month + Airline.Code + Scheduled.Departure.Hour + 
+    Type.of.Travel  + Shopping.Amount.at.Airport + 
+     + Class + Day.of.Month + Airline.Code + Scheduled.Departure.Hour + 
     Departure.Delay.in.Minutes.0 + Arrival.Delay.in.Minutes.0.Est + Flight.cancelled + 
     Flight.time.in.minutes.0.Est + Flight.Distance + Arrival.Delay.greater.5.Mins, 
     data = cleanSatisfactionSurvey))
@@ -22,3 +22,4 @@ summaryModel2
 # Orgin.City + Origin.State + Destination.City + Destination.State + Flight.date + Airline.Name
 
 # These were not statistically significant or less significant
+# X..of.Flight.with.other.Airlines + No..of.other.Loyalty.Cards + Eating.and.Drinking.at.Airport +
