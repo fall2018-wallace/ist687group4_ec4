1d3fccd812e93ec48b10056b5f3aa3788f38cab8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 10:03:57 -0500

    Saving summaryLM.R

--- summaryLM.R
+++ summaryLM.R
@@ -4,10 +4,7 @@ summaryModel <- summary(lm(formula = SatisfactionNumeric ~ Airline.Status + Age 
   Type.of.Travel + No..of.other.Loyalty.Cards + Shopping.Amount.at.Airport + Eating.and.Drinking.at.Airport +
   Class + Scheduled.Departure.Hour+ Arrival.Delay.greater.5.Mins, data = cleanSatisfactionSurvey))
 
-# This produced an error that we should look into
-# Flight.cancelled
 
-# These were not significant 
 # Airline.Code + Day.of.Month + Departure.Delay.in.Minutes + Arrival.Delay.in.Minutes + Flight.Distance +
 # 
 
