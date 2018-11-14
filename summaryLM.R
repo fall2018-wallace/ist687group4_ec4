a30bbf745ee5f8a326fba0ed33547fd5768405f7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 10:02:55 -0500

    Saving summaryLM.R

--- summaryLM.R
+++ summaryLM.R
@@ -4,9 +4,6 @@ summaryModel <- summary(lm(formula = SatisfactionNumeric ~ Airline.Status + Age 
   Type.of.Travel + No..of.other.Loyalty.Cards + Shopping.Amount.at.Airport + Eating.and.Drinking.at.Airport +
   Class + Scheduled.Departure.Hour+ Arrival.Delay.greater.5.Mins, data = cleanSatisfactionSurvey))
 
-# These were not applicable
-# Orgin.City + Origin.State + Destination.City + Destination.State + Flight.date + Airline.Name
-
 # This produced an error that we should look into
 # Flight.cancelled
 
@@ -18,9 +15,11 @@ summaryModel <- summary(lm(formula = SatisfactionNumeric ~ Airline.Status + Age 
 summaryModel2 <- summary(lm(formula = SatisfactionNumeric ~ Airline.Status + Age + Gender + 
     Price.Sensitivity + Year.of.First.Flight + No.of.Flights.p.a. + X..of.Flight.with.other.Airlines +
     Type.of.Travel + No..of.other.Loyalty.Cards + Shopping.Amount.at.Airport + 
-    Eating.and.Drinking.at.Airport + Class + Day.of.Month + Flight.date + Airline.Code + Airline.Name +
-    Orgin.City + Origin.State + Destination.City + Destination.State + Scheduled.Departure.Hour + 
+    Eating.and.Drinking.at.Airport + Class + Day.of.Month + Airline.Code + Scheduled.Departure.Hour + 
     Departure.Delay.in.Minutes.0 + Arrival.Delay.in.Minutes.0.Est + Flight.cancelled + 
     Flight.time.in.minutes.0.Est + Flight.Distance + Arrival.Delay.greater.5.Mins, 
     data = cleanSatisfactionSurvey))
 summaryModel2
+
+# These were not applicable or too specialized
+# Orgin.City + Origin.State + Destination.City + Destination.State + Flight.date + Airline.Name
