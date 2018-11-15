53f0173efc53e444c19f8ff44b13702b5a6041db
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 19:45:15 -0500

    Saving dSatisfactionSurveyPart4.R

--- dSatisfactionSurveyPart4.R
+++ dSatisfactionSurveyPart4.R
@@ -1,4 +1,4 @@
 
-dSatisfactionSurveyPart4<-df[,c("Airline.Status","Gender","Type.of.Travel","Shopping.Amount.at.Airport",
-"Class","Flight.date","Airline.Code","Airline.Name","Orgin.City","Origin.State","Destination.City",
-"Destination.State","Flight.cancelled","Arrival.Delay.greater.5.Mins","binarySat")]
+dSatisfactionSurveyPart4<-df[,c("Airline.Status","Gender","Type.of.Travel",
+"Class","Flight.date","Airline.Code","binarySat")]
+dSatisfactionSurveyPart4$
