508a9d9446c8409c69fe5b0b2e7c5dd8dee8f6b8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 21:39:14 -0500

    Saving associationrulesmining.R

--- associationrulesmining.R
+++ associationrulesmining.R
@@ -1,29 +1,8 @@
 
-library("arules")
-library("arulesViz")
-
-#Coerce the satisfaction data frame into a sparse transactions matrix 
-dSatisfactionX <- as(dSatisfactionCategory,"transactions")
-dSatisfactionX
-
-#Here we use the inspect( ), itemFrequency( ), and itemFrequencyPlot( ) commands to explore the contents of dSatisfactionX
-inspect(dSatisfactionX)
-itemFrequency(dSatisfactionX)
-itemFrequencyPlot(dSatisfactionX,cex.names=0.4)
-
-#Then we use arules to discover patterns
-#Run the apriori command to try and predict happy customers
-rules <- apriori(dSatisfactionX,parameter = list(support=0.2,confidence=0.6),appearance = list(default="lhs",rhs="binarySat=High"))
-summary(rules)
-inspect(rules)
-
-#Rank and comment
-rules.new <- rules[order(-quality(rules)$lift),]
-inspect(head(rules.new,5))  
 
 library(arules) 
 library(arulesViz)
-
+discretizeddata.csv
 #Coerce the satisfaction data frame into a sparse transactions matrix 
 dSatisfactionX <- as(df,"transactions")
 View(df)
