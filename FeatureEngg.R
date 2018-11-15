d820e41f58499e0dd4bd74ddd769e278003d08c3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 21:32:22 -0500

    Saving FeatureEngg.R

--- FeatureEngg.R
+++ FeatureEngg.R
@@ -1,7 +1,7 @@
 
 #Let's take an initial look at all our variables
 str(df)
-# 
+ 
 # 'data.frame':	129886 obs. of  35 variables:
 #   $ Satisfaction                    : num  4.5 4 2.5 4 5 5 3.5 4 4 4 ...
 # $ Airline.Status                  : Factor w/ 4 levels "Blue","Gold",..: 1 1 1 1 4 2 2 4 1 1 ...
