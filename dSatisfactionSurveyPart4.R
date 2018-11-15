f0bf92a391ebb6d435e88c057ced5a20a29f4b6a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 19:45:47 -0500

    Saving dSatisfactionSurveyPart4.R

--- dSatisfactionSurveyPart4.R
+++ dSatisfactionSurveyPart4.R
@@ -1,4 +1,6 @@
 
 dSatisfactionSurveyPart4<-df[,c("Airline.Status","Gender","Type.of.Travel",
 "Class","Flight.date","Airline.Code","binarySat")]
-dSatisfactionSurveyPart4$
+
+#Factorize variables
+dSatisfactionSurveyPart4$Airline.Status <- factor(dSatisfactionSurveyPart4$Airline.Status)
