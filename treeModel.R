7a6cfef6ff4367d460aff6b398258e459c0b2407
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-18 16:45:04 -0500

    Saving treeModel.R

--- treeModel.R
+++ treeModel.R
@@ -8,3 +8,41 @@ df$Airline.Code <- factor(df$Airline.Code)
 df$binarySat <- factor(df$binarySat)
 
 str(df)
+
+
+set.seed(101)
+samplerows <- sample(1:dim(df)[1])
+trainrows <- floor(2 * dim(df)[1]/3)
+train <- df[samplerows[1:trainrows],]
+test <- df[samplerows[(trainrows+1):dim(df)[1]],]
+
+library(tree)
+
+tree.model <- tree(binarySat~.,data = train)
+tree.model
+summary(tree.model)
+
+plot(tree.model)
+text(tree.model, pretty=0);
+
+tree.pred = predict(tree.model,test,type="class")
+
+confusionMatrix <- function(target,prediction){
+  confmatrix <- data.frame(target,prediction)
+  cm <- table(confmatrix)
+  return(cm) 
+}
+
+errorRate <- function(cm){
+  return(((cm[1,2]+cm[2,1])/sum(cm)))
+}
+
+cm <- confusionMatrix(test[,17],tree.pred)
+cm
+
+error <- errorRate(cm)
+error
+
+cv.tree.model = cv.tree(tree.model, FUN = prune.misclass)
+cv.tree.model
+plot(cv.tree.model)
