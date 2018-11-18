72e7544646730bb947f0469e55fa0edd1815f774
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-18 16:44:24 -0500

    Saving treeModel.R

--- treeModel.R
+++ treeModel.R
@@ -1,2 +1,10 @@
 
+ 
+df$Airline.Status <- factor(df$Airline.Status)
+df$Gender <- factor(df$Gender)
+df$Type.of.Travel <- factor(df$Type.of.Travel)
+df$Class <- factor(df$Class)
+df$Airline.Code <- factor(df$Airline.Code)
+df$binarySat <- factor(df$binarySat)
+
 str(df)
