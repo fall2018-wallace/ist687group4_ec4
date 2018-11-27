cddbc30753b68d0713abeef045f39bb8baf10f4a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-27 17:36:42 -0500

    Saving svmModeling.R

--- svmModeling.R
+++ svmModeling.R
@@ -28,10 +28,7 @@ trainData <- sampleData[randIndex[1:cutPoint2_3],]
 testData <- sampleData[randIndex[(cutPoint2_3+1):dim(sampleData)[1]],]
 
 #create the SVM expression with the following parameters
-svmOutput <- ksvm(binSat ~ Airline.Status + Age + Gender + 
-                    Price.Sensitivity + Year.of.First.Flight + No.of.Flights.p.a. + Type.of.Travel  + 
-                    Shopping.Amount.at.Airport + Class + Scheduled.Departure.Hour + Flight.cancelled + 
-                    Arrival.Delay.greater.5.Mins, data=sampleData, kernel="rbfdot", C=50, cross=3,
+svmOutput <- ksvm(binSat ~ Airline.Status + Age + Gender + Price.Sensitivity + No.of.Flights.p.a. + Type.of.Travel + Class + Departure.Delay.in.Minutes + Arrival.Delay.in.Minutes + Flight.time.in.minutes + Flight.Distance, data=sampleData, kernel="rbfdot", C=50, cross=3,
                   set.seed=1, prob.model=TRUE)
 svmOutput
 
