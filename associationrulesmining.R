64c3963815ce9b376ca6ff4b2dee225da873ec05
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-29 14:12:53 -0500

    Saving associationrulesmining.R

--- associationrulesmining.R
+++ associationrulesmining.R
@@ -8,19 +8,19 @@ dSatisfactionX <- as(dSatisfactionCategory,"transactions")
 
 
 #Here we use the inspect( ), itemFrequency( ), and itemFrequencyPlot( ) commands to explore the contents of dSatisfactionX
-inspect(dSatisfactionX)
+#inspect(dSatisfactionX)
 itemFrequency(dSatisfactionX)
 itemFrequencyPlot(dSatisfactionX,cex.names=0.4)
 
 #Then we use arules to discover patterns
 #Run the apriori command to try and predict happy customers
-rules <- apriori(dSatisfactionX,parameter = list(support=0.99,confidence=0.99),appearance = list(default="lhs",rhs="binarySat=High"))
-#summary(rules)
-#inspect(head(rules,5))  
+rules <- apriori(dSatisfactionX,parameter = list(support=0.2,confidence=0.6),appearance = list(default="lhs",rhs="binarySat=High"))
+summary(rules)
+inspect(rules)
 
 #Rank and comment
 rules.new <- rules[order(-quality(rules)$lift),]
-#inspect(head(rules.new,5))  
+inspect(head(rules.new,5))  
 print('done')
  
 #lhs                                 rhs                support confidence     lift count
