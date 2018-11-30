98e65fbfa107c71b68719f89f8b70c1b7b06e6e9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-30 17:57:20 -0500

    Saving associationrulesmining.R

--- associationrulesmining.R
+++ associationrulesmining.R
@@ -1,14 +1,14 @@
 
-
 library(arules) 
 library(arulesViz)
 library(methods)
 #Coerce the satisfaction data frame into a sparse transactions matrix 
+dSatisfactionCategory[] <- lapply(dSatisfactionCategory, factor)
 dSatisfactionX <- as(dSatisfactionCategory,"transactions")
 
-str(dSatisfactionX)
+
 #Here we use the inspect( ), itemFrequency( ), and itemFrequencyPlot( ) commands to explore the contents of dSatisfactionX
-inspect(dSatisfactionX)
+#inspect(dSatisfactionX)
 itemFrequency(dSatisfactionX)
 itemFrequencyPlot(dSatisfactionX,cex.names=0.4)
 
@@ -21,8 +21,8 @@ inspect(rules)
 #Rank and comment
 rules.new <- rules[order(-quality(rules)$lift),]
 inspect(head(rules.new,5))  
+print('done')
 
- 
 #lhs                                 rhs                support confidence     lift count
 #[1] {dArrDelay=Low,                                                                         
 #Type.of.Travel=Business travel} => {binarySat=High} 0.2596739  0.7597252 1.488082 33728
