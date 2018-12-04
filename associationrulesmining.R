983b02d65d993ae2cea599f2154af12291ab29da
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 19:13:38 -0500

    Saving associationrulesmining.R

--- associationrulesmining.R
+++ associationrulesmining.R
@@ -30,21 +30,28 @@ plot(rules.new.1,method="graph",main="Graph for 3 rules")
 rules.new.2<-rules.new[2]
 plot(rules.new.2,method="graph",main="Graph for 4 rules")
 
-#run the apriori command allowed us to predict happy customers.
-rules.high <- apriori(dSatisfactionX,parameter = list(support=0.3,confidence=0.5),appearance = list(default="lhs",rhs="binarySat=High"))
-summary(rules.high)
-inspect(rules.high)
+#lhs                                                       rhs             support   confidence lift    
+#[1] {Airline.Status=Blue,Class=Eco}                        => {binarySat=Low} 0.3263631 0.5885456  1.202439
+#[2] {dEating=Average,Airline.Status=Blue,Class=Eco}        => {binarySat=Low} 0.3263631 0.5885456  1.202439
+#[3] {dDayOfMonth=High,Airline.Status=Blue}                 => {binarySat=Low} 0.3081086 0.5840570  1.193268
+#[4] {dEating=Average,dDayOfMonth=High,Airline.Status=Blue} => {binarySat=Low} 0.3081086 0.5840570  1.193268
+#[5] {Airline.Status=Blue}                                  => {binarySat=Low} 0.3991500 0.5831131  1.191340
+
+
+#Run the apriori command to predict happy customers
+rulesHigh <- apriori(dSatisfactionX,parameter = list(support=0.3,confidence=0.5),appearance = list(default="lhs",rhs="binarySat=High"))
+summary(rulesHigh)
+inspect(rulesHigh)
 
 #Rank and comment
 #Use following code allowed to rank the resulting rules
-rules.high <- rules[order(-quality(rules)$lift),]
-inspect(head(rules.high,5))  
+rules.old <- rulesHigh[order(-quality(rulesHigh)$lift),]
+inspect(head(rules.old,5))  
 #Write code to visualize the two highest satisfaction rule
-rules.high.1<-head(rules.high,1)
-plot(rules.high.1,method="graph",main="Graph for 3 rules")
-rules.high.2<-rules.high[2]
-plot(rules.high.2,method="graph",main="Graph for 4 rules")
-
+rules.old.1<-head(rules.old,1)
+plot(rules.old.1,method="graph",main="Graph for 3 rules")
+rules.old.2<-rules.old[2]
+plot(rules.old.2,method="graph",main="Graph for 4 rules")
  #lhs                                                           rhs            support   confidence lift    
 #[1] {Airline.Status=Blue,Class=Eco}                        => {binarySat=Low} 0.3263631 0.5885456  1.202439
 #[2] {dEating=Average,Airline.Status=Blue,Class=Eco}        => {binarySat=Low} 0.3263631 0.5885456  1.202439
