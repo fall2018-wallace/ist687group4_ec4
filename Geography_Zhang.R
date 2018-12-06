73db77d92f63b9ec2fe86c659ba03b670014b1f8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-06 11:31:35 -0500

    Saving Geography_Zhang.R

--- Geography_Zhang.R
+++ Geography_Zhang.R
@@ -37,10 +37,6 @@ oStateMap <- oStateMap+expand_limits(x = us$long, y = us$lat)
 oStateMap <- oStateMap+coord_map() +ggtitle ("Map of the Origine State")
 oStateMap
 
-ggplot(map_origin,aes (x=reorder(Origin.State,-average_sat),y=average_sat))+
-  geom_col (fill="blue", color="black") + 
-  theme(axis.text = element_text(angle = 90, hjust=1))
-
 #Create a color coded map, based on the Destination State
 map_destination <- surveyGeo %>% 
   group_by(Destination.State) %>% 
